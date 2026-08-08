0eda39b5f65ba3f930480c34bd4124a2daec7dcf
clintonasokwonye-wq <clintonasokwonye@gmail.com>
2026-08-07 22:42:54 -0700

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -1,15 +1,14 @@
-console.log('custom.js ----')
-
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded')
-})
-
-// 监听全屏变化
-document.addEventListener('fullscreenchange', () => {
-    console.log('fullscreenchange')
-})
-
-// Safari / WebKit
-document.addEventListener('webkitfullscreenchange', () => {
-    console.log('webkitfullscreenchange')
-})
+window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("script");t.src="https://www.googletagmanager.com/gtag/js?id=G-W5GKHM0893",t.async=!0,document.head.appendChild(t);const n=document.createElement("script");n.textContent="window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-W5GKHM0893');",document.body.appendChild(n)});window.addEventListener('DOMContentLoaded', () => {
+    // Look for an existing theme color
+    let metaThemeColor = document.querySelector('meta[name="theme-color"]');
+    
+    // If it doesn't exist, create it
+    if (!metaThemeColor) {
+        metaThemeColor = document.createElement('meta');
+        metaThemeColor.name = 'theme-color';
+        document.head.appendChild(metaThemeColor);
+    }
+    
+    // Force the color to dark grey
+    metaThemeColor.content = '#000000';
+});
